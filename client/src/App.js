2c896930a10f9d0513bced2aeb9d142be74c63ec
AryanVashistha <aryanvashisthajammu@gmail.com>
2023-08-23 11:30:48 +0530

    Edit Files

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { uploadFile } from './service/api';
+import logo from './image.png';    //This is how we import the required images from the directory
+
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+  <div className="parent">
+    <div className='container'>
+      <img src={logo} className='img' alt="banner" id="logoimg"/>
+      <div className='wrapper'>
+        <h1>Simple file sharing!</h1>
+        <p>Upload and share the download link.</p>
+        
+        <button onClick={() => onUploadClick()}>Upload</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
+        <a href={result} target='_blank'>{result}</a> 
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
